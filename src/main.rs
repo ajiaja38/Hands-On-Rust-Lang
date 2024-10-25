c4c6911624503dcefa46b11d9edf4f5ec85f0e71
M. Aji Perdana <ajicooljazz38@gmail.com>
2024-10-25 14:39:50 +0700

    feat: add shadowing ✨

--- src/main.rs
+++ src/main.rs
@@ -13,3 +13,30 @@ fn test_variable() {
     
     println!("Hello, {}", name)
 }
+
+#[test]
+fn test_variable_mutable() {
+    let mut name: &str = "Frasiska Risma Yolanda";
+    println!("Hello, {}", name);
+
+    name = "Eyyyinnn Nopasiaaa 😹";
+    println!("Hello, {}", name)
+}
+
+#[test]
+fn test_static_typing() {
+    let name: &str = "Hello Static type";
+    println!("Hello, {}", name);
+
+    // name = 32;
+    println!("Hello, {}", name)
+}
+
+#[test]
+fn shadowin() {
+    let var: &str = "M. Aji Perdana";
+    println!("Hello, {}", var);
+
+    let var: i32 = 10;
+    println!("Ini adalah angka: {}", var)
+}
