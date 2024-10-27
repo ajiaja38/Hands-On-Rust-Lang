5f9c5222ad823e2c57728a5830d34d4a65f3de9e
M. Aji Perdana <ajicooljazz38@gmail.com>
2024-10-27 11:10:52 +0700

    feat: update kuyyinnn ✨

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn test_variable_mutable() {
     let mut name: &str = "Erlin Novasia";
     println!("Hello, {}", name);
 
-    name = "Eyyyinnn Nopasiaaa 😹";
+    name = "Eyyyinnn Nopasiaaaa 😹";
     println!("Hello, {}", name)
 }
 
